946fa418c3a7b278643d2bae8faeed0e2590daa1
Eslam Halawany <halawany.me@gmail.com>
2023-05-02 06:23:22 +0300

    Finished the homepage

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false
+    });
+  });
