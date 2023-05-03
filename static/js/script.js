5181d2a8c060e495852e512a89169344bc5c6b10
Eslam Halawany <halawany.me@gmail.com>
2023-05-03 07:40:06 +0300

    finished course detail page

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-    $('.testimonial-slider').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      arrows: false
-    });
+// Add event listener to navbar links to scroll to sections
+const navLinks = document.querySelectorAll('.main-nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+    const targetTitle = targetSection.querySelector('h2').innerText;
+    document.title = targetTitle;
+    window.history.pushState(null, null, targetId);
   });
+});
