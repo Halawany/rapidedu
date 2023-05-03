4eff0dafbb6e4d9ccecb288a296b5e8ce67d5832
Eslam Halawany <halawany.me@gmail.com>
2023-05-03 05:53:33 +0300

    Finished courses page layout

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('courses', views.CoursesListView.as_view(), name='courses'),
+    path('course/<int:pk>', views.CourseDetailView.as_view(), name='course_detail'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+from .models import Course
 
 class HomeView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'courses/homepage.html'
+
+class CoursesListView(ListView):
+    model = Course
+    context_object_name = 'courses'
+    template_name = 'courses/courses.html'
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course_detail.html'
